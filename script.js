8e238f563102212f673552970b52b8d013ed3339
satish <rahul>
2023-03-11 08:09:36 +0530

    first

---
+++ script.js
@@ -0,0 +1,28 @@
+
+function cal(){
+    var opr1 = document.getElementById("first").value;
+    var opr2 = document.getElementById("second").value;
+    var ope=document.getElementById("ope").value;
+    if(ope=="+"){
+        var res= parseInt(opr1) +parseInt(opr2)  ;
+
+
+    }
+    if(ope=="-"){
+        var res= parseInt(opr1)  - parseInt(opr2)  ;
+
+
+    }
+    if(ope=="*"){
+        var res= parseInt(opr1)  *parseInt(opr2)  ;
+
+
+    }
+    if(ope=="/"){
+        var res= parseInt(opr1) /parseInt(opr2)  ;
+
+
+    }
+  var res=document.getElementById("res").value=res;
+
+}
